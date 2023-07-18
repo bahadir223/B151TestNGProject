15c48ee7a7e81f11d00dd38aa4b4aa0ec788db3b
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-07-18 20:51:35 +0300

    day26_PageKullanimi package'inda calisma yapildi.

---
+++ src/test/java/techproed/tests/day26_PageKullanimi/C01_PageKullanimi.java
@@ -0,0 +1,19 @@
+package techproed.tests.day26_PageKullanimi;
+
+import org.testng.annotations.Test;
+import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
+
+public class C01_PageKullanimi {
+    @Test
+    public void test01() {
+        //https://opensource-demo.orangehrmlive.com/web/index.php/auth/login adrese gidelim
+        Driver.getDriver().get(ConfigReader.getProperty("opensourceUrl"));
+        //Username : Admin
+        //Password : admin123
+
+
+        //kullaniciAdi, kullaniciSifre, submitButton elementlerini locate edelim
+        //Login Testinin basarili oldugunu test et
+    }
+}
