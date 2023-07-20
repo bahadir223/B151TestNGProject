dd745ec8f935604a46027e964113f7e3e3bd4d5f
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-07-20 20:42:43 +0300

    day27_SmokeTest_Excel package'i olusturuldu.

--- src/test/java/techproed/tests/day27_SmokeTest_Excel/C01_NegatifTest.java
+++ src/test/java/techproed/tests/day27_SmokeTest_Excel/C01_NegatifTest.java
@@ -1,4 +1,16 @@
 package techproed.tests.day27_SmokeTest_Excel;
 
+import org.testng.annotations.Test;
+
 public class C01_NegatifTest {
+    @Test
+    public void test01() {
+        //Description:
+        //Kullanimda olmayan kullanıcı adi ve şifre ile giriş yapilamamali
+        //Acceptance Criteria
+        //Customer email: fake@bluerentalcars.com
+        //Customer password: fakepass
+        //Error:
+        //User with email fake@bluerentalcars.com not found
+    }
 }
