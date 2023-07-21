0f739a322bc7b5e386b5f380582122e141281606
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-07-21 20:58:15 +0300

    C01_DataProviderTest class'inda calisma yapildi.

--- src/test/java/techproed/tests/day28_DataProvider/C01_DataProviderTest.java
+++ src/test/java/techproed/tests/day28_DataProvider/C01_DataProviderTest.java
@@ -1,4 +1,25 @@
 package techproed.tests.day28_DataProvider;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class C01_DataProviderTest {
+
+    /*
+    Dataprovider, birçok veriyi test caselerde loop kullanmadan aktarmak icin kullanilir.
+    TestNG'den gelen bir özelliktir.
+    2 boyutlu Object Array return eder.
+    Kullanımı icin @Test notasyonundan sonra parametre olarak (dataprovider="method ismi") yazilir.
+    Kaç tane veri ile calisacaksak test methoduna o kadar parametre eklenir.
+     */
+
+    @DataProvider
+    public static Object[][] isimler() {
+        return new Object[][]{{"esen"}, {"mehmet"}, {"esma"}, {"ali"}, {"mert"}, {"burcu"}, {"yunus"}};
+    }
+
+    @Test(dataProvider = "isimler")
+    public void test01(String data) {
+        System.out.println(data);
+    }
 }
